cd7a195c44c6d71c4680323d8406deebe5be7063
SudarshnaThakur15 <119600694+SudarshnaThakur15@users.noreply.github.com>
2024-08-14 13:53:00 +0530

    cors update

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,13 @@ const port = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json()); 
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'http://localhost:5173/',
+    credentials: true,
+  }
+
+));
 
 // Connect to the database
 (async () => {
